5bccf15663b641361715a687989685caf9a49fa9
AtharvaDeokar21 <atharva.deokar2005@gmail.com>
2024-07-31 16:44:04 +0530

    Styled few authentication pages

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    './users/templates/**/*.html',
+    './users/templates/**/*.js',
+    './users/static/users/**/*.css',
+    './users/static/users/**/*.js',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
